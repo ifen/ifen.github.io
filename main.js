66df4e0c794c592274a74477f254818314fc828c
Ian Fenech Conti <ianfc89@gmail.com>
2014-01-31 00:20:24 +0100

    Added Webpages

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+  var on = false;
+  $(".menu-button").click(function(){
+    if(on == false){
+      $(".menu-bar").addClass("open");
+      on = true;
+    } else {
+      $(".menu-bar").removeClass("open");
+      on = false;
+    }
+  });
+});
+
+window.open    = function(){};
+window.print   = function(){};
+      // Support hover state for mobile.
+      if (false) {
+        window.ontouchstart = function(){};
+      }
+
