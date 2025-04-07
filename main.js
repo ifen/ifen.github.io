42799105884dafc3a70db43ebc69590fffd65792
Ian Fenech Conti <ianfc89@gmail.com>
2025-04-07 15:31:26 +0200

    Added Sitemap

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth"
     });
+    // Also update URL hash without page jump
+    history.pushState(null, null, this.getAttribute("href"));
   });
 });
